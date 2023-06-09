c5b54e93cfe26e1c968dde55da1eaf83735e4b48
Estelson Medeiros Pereira <estelson@gmail.com>
2023-06-08 21:19:03 -0300

    Seção 4: Welcome screen, stack and column layout widgets ui - parts 1 and 2
    Dependências:
    - flutter_screenutil: ^5.8.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ulearning_app/app_blocs.dart';
-import 'package:ulearning_app/app_events.dart';
-import 'package:ulearning_app/app_states.dart';
+import 'package:ulearning_app/pages/welcome/welcome.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AppBlocs(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter Demo Home Page"),
-        centerTitle: true,
-        // Cor da StatusBar em dispositivos mobile
-        systemOverlayStyle: const SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarIconBrightness: Brightness.light,
+      child: ScreenUtilInit(
+        builder: (context, child) => const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Welcome(),
         ),
       ),
-      body: Center(child: BlocBuilder<AppBlocs, AppStates>(
-        builder: (context, state) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                // "${BlocProvider.of<AppBlocs>(context).state.counter}",
-                "${state.counter}",
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          );
-        },
-      )),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          FloatingActionButton(
-            onPressed: () => BlocProvider.of<AppBlocs>(context).add(
-              Decrement(),
-            ),
-            tooltip: 'Decrement',
-            child: const Icon(Icons.remove),
-          ),
-          FloatingActionButton(
-            onPressed: () => BlocProvider.of<AppBlocs>(context).add(
-              Increment(),
-            ),
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-        ],
-      ),
     );
   }
 }
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Flutter Demo Home Page"),
+//         centerTitle: true,
+//         // Cor da StatusBar em dispositivos mobile
+//         systemOverlayStyle: const SystemUiOverlayStyle(
+//           statusBarColor: Colors.transparent,
+//           statusBarIconBrightness: Brightness.light,
+//         ),
+//       ),
+//       body: Center(child: BlocBuilder<AppBlocs, AppStates>(
+//         builder: (context, state) {
+//           return Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               const Text(
+//                 'You have pushed the button this many times:',
+//               ),
+//               Text(
+//                 // "${BlocProvider.of<AppBlocs>(context).state.counter}",
+//                 "${state.counter}",
+//                 style: Theme.of(context).textTheme.headlineMedium,
+//               ),
+//             ],
+//           );
+//         },
+//       )),
+//       floatingActionButton: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: [
+//           FloatingActionButton(
+//             onPressed: () => BlocProvider.of<AppBlocs>(context).add(
+//               Decrement(),
+//             ),
+//             tooltip: 'Decrement',
+//             child: const Icon(Icons.remove),
+//           ),
+//           FloatingActionButton(
+//             onPressed: () => BlocProvider.of<AppBlocs>(context).add(
+//               Increment(),
+//             ),
+//             tooltip: 'Increment',
+//             child: const Icon(Icons.add),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.only(top: 34.h),
+          width: 375.w,
+          child: Stack(
+            children: [
+              PageView(
+                physics: const BouncingScrollPhysics(),
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: 345.w,
+                        height: 375.w,
+                        child: const Text("Image one"),
+                      ),
+                      Container(
+                        child: Text(
+                          "First see learning",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24.sp,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "Forget about a for of paper all knowledge in one learning",
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(0.5),
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
