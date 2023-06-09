b9079d82841d15011c1d53b41646db05fdc5c080
Estelson Medeiros Pereira <estelson@gmail.com>
2023-06-09 09:22:33 -0300

    Seção 4: MultiBlocProviders - part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/app_blocs.dart';
+import 'package:ulearning_app/app_events.dart';
+import 'package:ulearning_app/app_states.dart';
 import 'package:ulearning_app/pages/welcome/bloc/welcome_blocs.dart';
 import 'package:ulearning_app/pages/welcome/welcome.dart';
 
@@ -13,8 +17,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => WelcomeBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => WelcomeBloc()),
+        BlocProvider(create: (context) => AppBlocs()),
+      ],
       child: ScreenUtilInit(
         builder: (context, child) => const MaterialApp(
           debugShowCheckedModeBanner: false,
@@ -25,57 +32,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Flutter Demo Home Page"),
-//         centerTitle: true,
-//         // Cor da StatusBar em dispositivos mobile
-//         systemOverlayStyle: const SystemUiOverlayStyle(
-//           statusBarColor: Colors.transparent,
-//           statusBarIconBrightness: Brightness.light,
-//         ),
-//       ),
-//       body: Center(child: BlocBuilder<AppBlocs, AppStates>(
-//         builder: (context, state) {
-//           return Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               const Text(
-//                 'You have pushed the button this many times:',
-//               ),
-//               Text(
-//                 // "${BlocProvider.of<AppBlocs>(context).state.counter}",
-//                 "${state.counter}",
-//                 style: Theme.of(context).textTheme.headlineMedium,
-//               ),
-//             ],
-//           );
-//         },
-//       )),
-//       floatingActionButton: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceAround,
-//         children: [
-//           FloatingActionButton(
-//             onPressed: () => BlocProvider.of<AppBlocs>(context).add(
-//               Decrement(),
-//             ),
-//             tooltip: 'Decrement',
-//             child: const Icon(Icons.remove),
-//           ),
-//           FloatingActionButton(
-//             onPressed: () => BlocProvider.of<AppBlocs>(context).add(
-//               Increment(),
-//             ),
-//             tooltip: 'Increment',
-//             child: const Icon(Icons.add),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Demo Home Page"),
+        centerTitle: true,
+        // Cor da StatusBar em dispositivos mobile
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.light,
+        ),
+      ),
+      body: Center(child: BlocBuilder<AppBlocs, AppStates>(
+        builder: (context, state) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                // "${BlocProvider.of<AppBlocs>(context).state.counter}",
+                "${state.counter}",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          );
+        },
+      )),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            heroTag: "heroTag1",
+            onPressed: () => BlocProvider.of<AppBlocs>(context).add(
+              Decrement(),
+            ),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            heroTag: "heroTag2",
+            onPressed: () => BlocProvider.of<AppBlocs>(context).add(
+              Increment(),
+            ),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -2,6 +2,7 @@ import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/main.dart';
 import 'package:ulearning_app/pages/welcome/bloc/welcome_blocs.dart';
 import 'package:ulearning_app/pages/welcome/bloc/welcome_events.dart';
 import 'package:ulearning_app/pages/welcome/bloc/welcome_states.dart';
@@ -136,11 +137,14 @@ class _WelcomeState extends State<Welcome> {
               // transition animation
               pageController.animateToPage(
                 index,
-                duration: const Duration(milliseconds: 1000),
+                duration: const Duration(milliseconds: 500),
                 curve: Curves.easeIn,
               );
             } else {
               // jump to a new page
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const MyHomePage()),
+              );
             }
           },
           child: Container(
