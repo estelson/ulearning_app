cbdccf8b9d1b964a90bd721e857750791b46645b
Estelson Medeiros Pereira <estelson@gmail.com>
2023-06-12 19:17:08 -0300

    Seção 9: Register screen and register states - part 5

---
+++ lib/pages/register/bloc/register_states.dart
@@ -0,0 +1,8 @@
+class RegisterStates {
+  String userName;
+  String email;
+  String password;
+  String rePassword;
+
+  RegisterStates(this.userName, this.email, this.password, this.rePassword);
+}
