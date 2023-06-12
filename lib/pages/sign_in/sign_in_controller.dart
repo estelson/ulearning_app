0d06170f8dd1ce02ea60c1bb3e103fad36272227
Estelson Medeiros Pereira <estelson@gmail.com>
2023-06-12 11:38:29 -0300

    Seção 8: SignIn controller and Firebase Auth - part 3
    Dependências:
    - firebase_auth: ^4.6.2

--- lib/pages/sign_in/sign_in_controller.dart
+++ lib/pages/sign_in/sign_in_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ulearning_app/pages/sign_in/bloc/sign_in_blocs.dart';
@@ -12,14 +13,29 @@ class SignInController {
   /// - Apple account
   /// - Facebook account
   /// - Login with email and password.
-  void handleSignIn(String type) {
+  Future<void> handleSignIn(String type) async {
     try {
-      if(type=="email") {
+      if (type == "email") {
         // final state = BlocProvider.of<SignInBloc>(context).state;
         final state = context.read<SignInBloc>().state;
-      }
-    } catch (e) {
+        String emailAddress = state.email;
+        String password = state.password;
 
-    }
+        if (emailAddress.isEmpty) {
+          ///
+        }
+
+        if (password.isEmpty) {
+          ///
+        }
+
+        try {
+          final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+            email: emailAddress,
+            password: password,
+          );
+        } catch (e) {}
+      }
+    } catch (e) {}
   }
 }
