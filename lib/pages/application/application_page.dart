098af9395a6156f6b8fa5f208dc0ed1112be2abd
Estelson Medeiros Pereira <estelson@gmail.com>
2023-06-13 15:05:44 -0300

    Seção 11: Application screen and bottom navigation bar - part 3

--- lib/pages/application/application_page.dart
+++ lib/pages/application/application_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/common/values/colors.dart';
 import 'package:ulearning_app/pages/application/widgets/application_widgets.dart';
 
 class ApplicationPage extends StatefulWidget {
@@ -10,6 +11,8 @@ class ApplicationPage extends StatefulWidget {
 }
 
 class _ApplicationPageState extends State<ApplicationPage> {
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,9 +20,20 @@ class _ApplicationPageState extends State<ApplicationPage> {
       child: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
-          body: buildPage(1),
+          body: buildPage(_index),
           bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _index,
+            onTap: (value) {
+              setState(() {
+                _index = value;
+              });
+            },
             elevation: 0,
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            selectedItemColor: AppColors.primaryElement,
+            unselectedItemColor: AppColors.primaryFourthElementText,
             backgroundColor: Colors.white,
             items: [
               BottomNavigationBarItem(
@@ -37,6 +51,30 @@ class _ApplicationPageState extends State<ApplicationPage> {
                   child: Image.asset("assets/icons/home.png"),
                 ),
                 label: "Search",
+              ),
+              BottomNavigationBarItem(
+                icon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset("assets/icons/home.png"),
+                ),
+                label: "Course",
+              ),
+              BottomNavigationBarItem(
+                icon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset("assets/icons/home.png"),
+                ),
+                label: "Chat",
+              ),
+              BottomNavigationBarItem(
+                icon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset("assets/icons/home.png"),
+                ),
+                label: "Profile",
               ),
             ],
           ),
