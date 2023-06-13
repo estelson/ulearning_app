e0e9e1e652a2589e71fca2aebfb400aed4fe637d
Estelson Medeiros Pereira <estelson@gmail.com>
2023-06-13 18:39:32 -0300

    Seção 11: Application screen and ui bottom navigation part 4Application screen and ui bottom navigation - part 4

--- lib/pages/application/application_page.dart
+++ lib/pages/application/application_page.dart
@@ -34,7 +34,6 @@ class _ApplicationPageState extends State<ApplicationPage> {
             showUnselectedLabels: false,
             selectedItemColor: AppColors.primaryElement,
             unselectedItemColor: AppColors.primaryFourthElementText,
-            backgroundColor: Colors.white,
             items: [
               BottomNavigationBarItem(
                 icon: SizedBox(
@@ -43,38 +42,78 @@ class _ApplicationPageState extends State<ApplicationPage> {
                   child: Image.asset("assets/icons/home.png"),
                 ),
                 label: "Home",
+                activeIcon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset(
+                    "assets/icons/home.png",
+                    color: AppColors.primaryElement,
+                  ),
+                ),
               ),
               BottomNavigationBarItem(
                 icon: SizedBox(
                   width: 15.w,
                   height: 15.h,
-                  child: Image.asset("assets/icons/home.png"),
+                  child: Image.asset("assets/icons/search2.png"),
                 ),
                 label: "Search",
+                activeIcon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset(
+                    "assets/icons/search2.png",
+                    color: AppColors.primaryElement,
+                  ),
+                ),
               ),
               BottomNavigationBarItem(
                 icon: SizedBox(
                   width: 15.w,
                   height: 15.h,
-                  child: Image.asset("assets/icons/home.png"),
+                  child: Image.asset("assets/icons/play-circle1.png"),
                 ),
                 label: "Course",
+                activeIcon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset(
+                    "assets/icons/play-circle1.png",
+                    color: AppColors.primaryElement,
+                  ),
+                ),
               ),
               BottomNavigationBarItem(
                 icon: SizedBox(
                   width: 15.w,
                   height: 15.h,
-                  child: Image.asset("assets/icons/home.png"),
+                  child: Image.asset("assets/icons/message-circle.png"),
                 ),
                 label: "Chat",
+                activeIcon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset(
+                    "assets/icons/message-circle.png",
+                    color: AppColors.primaryElement,
+                  ),
+                ),
               ),
               BottomNavigationBarItem(
                 icon: SizedBox(
                   width: 15.w,
                   height: 15.h,
-                  child: Image.asset("assets/icons/home.png"),
+                  child: Image.asset("assets/icons/person2.png"),
                 ),
                 label: "Profile",
+                activeIcon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset(
+                    "assets/icons/person2.png",
+                    color: AppColors.primaryElement,
+                  ),
+                ),
               ),
             ],
           ),
