4e8d240f7daacb2b2d7478a70cfbe363f4ceba46
Estelson Medeiros Pereira <estelson@gmail.com>
2023-06-13 22:24:38 -0300

    Seção 12: Route section and understand bloc and routes - part 1

---
+++ lib/common/routes/names.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: constant_identifier_names
+
+class AppRoutes {
+  /// Welcome page or on boarding
+  static const INITIAL = "/";
+
+  /// Application page
+  static const APPLICATION = "/application";
+
+  /// SignIn page
+  static const SIGN_IN = "/sign_in";
+
+  /// Register page
+  static const REGISTER = "/register";
+}
