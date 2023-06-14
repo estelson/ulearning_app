e0feec2da46ea32393af184542926bb3e7ef19d7
Estelson Medeiros Pereira <estelson@gmail.com>
2023-06-14 15:52:01 -0300

    Seção 13:  SharedPreferences and remember the welcome screen - part 4

--- lib/common/routes/pages.dart
+++ lib/common/routes/pages.dart
@@ -56,8 +56,16 @@ class AppPages {
       /// Check for route name matching when navigator gets triggered
       var result = routes().where((element) => element.route == settings.name);
       if (result.isNotEmpty) {
+        debugPrint("First login");
+        debugPrint(result.first.route);
+
         bool deviceFirstOpen = Global.storageService.getDeviceFirstOpen();
-        if(result.first.route == AppRoutes.INITIAL && deviceFirstOpen)
+        if(result.first.route == AppRoutes.INITIAL && deviceFirstOpen) {
+          debugPrint("second login");
+
+
+          return MaterialPageRoute(builder: (_) => const SignIn(), settings: settings);
+        }
 
         return MaterialPageRoute(builder: (_) => result.first.page, settings: settings);
       }
