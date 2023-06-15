4136b48f802fa7ad572e81e601d4a67425f0b0b6
Estelson Medeiros Pereira <estelson@gmail.com>
2023-06-15 15:34:45 -0300

    Seção 14: Profile page and dynamic listview - part 5

--- lib/pages/profile/widgets/profile_widgets.dart
+++ lib/pages/profile/widgets/profile_widgets.dart
@@ -54,36 +54,47 @@ Widget profileIconAndEditButton() {
 }
 
 /// Setting sections buttons
+var imagesInfo = <String, String>{
+  "Settings": "settings.png",
+  "Payment details": "credit-card.png",
+  "Achievement": "award.png",
+  "Love": "heart_1.png",
+  "Learning reminders": "cube.png",
+};
+
 Widget buildListView() {
   return Column(
     children: [
-      GestureDetector(
-        child: Container(
-          child: Row(
-            children: [
-              Container(
-                width: 40.w,
-                height: 40.h,
-                padding: const EdgeInsets.all(7.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.w),
-                  color: AppColors.primaryElement,
+      ...List.generate(
+        imagesInfo.length,
+        (index) => GestureDetector(
+          child: Container(
+            margin: EdgeInsets.only(bottom: 15.h),
+            child: Row(
+              children: [
+                Container(
+                  width: 40.w,
+                  height: 40.h,
+                  padding: const EdgeInsets.all(7.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.w),
+                    color: AppColors.primaryElement,
+                  ),
+                  child: Image.asset("assets/icons/${imagesInfo.values.elementAt(index)}"),
                 ),
-                child: Image.asset("assets/icons/settings.png"),
-              ),
-              SizedBox(width: 15.w),
-              Text(
-                "Settings",
-                style: TextStyle(
-                  color: AppColors.primaryText,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.sp,
+                SizedBox(width: 15.w),
+                Text(
+                  imagesInfo.keys.elementAt(index),
+                  style: TextStyle(
+                    color: AppColors.primaryText,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.sp,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-
       ),
     ],
   );
